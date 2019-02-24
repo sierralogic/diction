8dfda8369cfbde881cd69b1a5973d48329e0b435
Greg Seaton <greg.seaton@sierralogic.com>
2019-02-24 09:59:10 -0800

    updated example

--- src/diction/example.clj
+++ src/diction/example.clj
@@ -1,22 +1,31 @@
 (ns diction.example
-  (:require [diction.core :refer [int! float! string! uuid! joda! element! document!
-                                  enum! inherit! clone! vector! keyword! generate-random-uuid]:as diction]))
+  (:require [diction.core :refer [generate int! float! string! uuid! joda! element! document!
+                                  enum! inherit! clone! double! long! vector! keyword! generate-random-uuid]
+             :as diction]))
 
-;;; inventory
-
-(uuid! ::id)
-(int! ::unit-count {:min 0 :max 999})
-(float! ::unit-cost {:min 0.0 :max 999.99 :decimal-places 2})
 (string! ::label {:min 0 :max 8})
+
 (string! ::description {:min 0 :max 128})
+
+(long! ::unit-count {:min 0 :max 999})
+(double! ::unit-cost {:min 0.01 :max 99.99})
+
 (string! ::tag {:min 2 :max 12})
+
 (vector! ::tags ::tag)
+
 (keyword! ::badge {:max 8})
+
 (joda! ::created)
 
-(float! ::percent-upcharge {:min 0.0 :max 1.0})
-(float! ::fee-upcharge {:min 0.0 :max 999.99})
+(double! ::percent-upcharge {:min 0.0 :max 1.0})
+
+(double! ::fee-upcharge {:min 0.0 :max 999.99})
+
+(uuid! ::id)
+
 (enum! ::cost-basis #{:flat :per-unit})
+
 
 (document! ::additional-charges-fee
            [::label ::fee-upcharge ::cost-basis]
@@ -37,3 +46,4 @@
 (document! ::item
            [::id ::label ::unit-count ::unit-cost]
            [::badge ::tags ::description ::additional-charges])
+
