5bbf4431d165d772a75ea767fb2434d9e0f4c6e7
G Seaton <greg.seaton@sierralogic.com>
2019-03-01 12:19:13 -0800

    removed unneeded deps in project

--- project.clj
+++ project.clj
@@ -3,9 +3,6 @@
   :url "http://sierralogic.com/project/diction"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ;:dependencies [[org.clojure/clojure "1.9.0"]
-  ;               [clj-time "0.15.0"]
-  ;               [com.velisco/strgen "0.1.8"]]
   :plugins [[lein-tools-deps "0.4.3"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
