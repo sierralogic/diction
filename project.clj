4f5fd7c4eda6e1711948d2c99461638f4b4ef912
G Seaton <greg.seaton@sierralogic.com>
2019-03-01 12:15:14 -0800

    lein deps test

--- project.clj
+++ project.clj
@@ -3,8 +3,11 @@
   :url "http://sierralogic.com/project/diction"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [clj-time "0.15.0"]
-                 [com.velisco/strgen "0.1.8"]]
+  ;:dependencies [[org.clojure/clojure "1.9.0"]
+  ;               [clj-time "0.15.0"]
+  ;               [com.velisco/strgen "0.1.8"]]
+  :plugins [[lein-tools-deps "0.4.3"]]
+  :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
+  :lein-tools-deps/config {:config-files [:install :user :project]}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
