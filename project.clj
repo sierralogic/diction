59d6684f36ce6e4bdbcebe646b118a2a5d8ba3b3
gseaton <greg.seaton@sierralogic.com>
2019-05-04 17:56:46 -0700

    cleaned up project

--- project.clj
+++ project.clj
@@ -6,8 +6,5 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-time "0.15.0"]
                  [com.velisco/strgen "0.1.8"]]
-  :plugins []
-  :middleware []
-  :lein-tools-deps/config {:config-files [:install :user :project]}
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
