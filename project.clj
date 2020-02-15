0be994652d2f2556dd63147322799f153e6d7eae
Greg Seaton <greg.seaton@sierralogic.com>
2020-02-15 08:37:47 -0800

    added sierralogic gpr package repo

--- project.clj
+++ project.clj
@@ -6,5 +6,8 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [clj-time "0.15.0"]
                  [com.velisco/strgen "0.1.8"]]
+  :repositories [["github" {:url "https://maven.pkg.github.com/sierralogic/gpr"
+                            :username [:gpg :env/github_username]
+                            :password [:gpg :env/github_password]}]]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
