31c5b314bcb7ac41b6caeb5361c096500b6e5522
G Seaton <greg.seaton@sierralogic.com>
2020-03-10 11:28:59 -0700

    updated the demo dictionary

--- src/diction/demo.clj
+++ src/diction/demo.clj
@@ -13,138 +13,6 @@
             [diction.documentation :as doc]
             [diction.util :as util]))
 
-
-;;x_id : MongoDB OID unique id
-;xactive : true|false
-
-;additional_charges : list of addt'l charges
-  ;amount : decimal
-  ;charge_id : string/oid; generated but not used outside of item
-  ;charge_type : string; tax | fee
-  ;charge_unit : string; % | $
-  ;label : string; short description of addt'l charge
-
-;group_id : string
-;min_count : integer; minimal count required to purchase
-;space_id : string; MongoDB oid; item associated to space
-;sso_id : string; MongoDB sso id of owner/vendor; all one's 111...111 for Peerspace add-ons/concierge
-;is_concierge : true|false; Peerspace concierge convenience flag
-
-
-;xcategory : string
-;xdescription : string
-;ximage_ids : list of strings; cloudinary image ids
-;xlabel : string; (AT: name)
-;xsubcategory : string
-;xunit_cost : decimal; cost per unit
-;xunit_cost_basis : string; per_person | per_unit | ?
-;xvisible_on_listing : true|false
-
-;Example
-
-;(string! :moid {:min 24 :max 24 :chars "abcdef0123456789" :meta {:hint "24-char MongoDB string id"}})
-;
-;(clone! :moid :_id)
-;
-;(string! :label {:meta {:sensible-values ["label1" "label2" "label zed"]}})
-;(string! :description {:meta {:sensible-values ["Something descriptive" "Another description" "This is descriptive"]}})
-;
-;(boolean! :active)
-;
-;(string! :category {:meta {:sensible-values ["cata" "catb" "catz"]}})
-;
-;(string! :subcategory {:meta {:sensible-values ["subcat1" "subcat2" "subcat99"]}})
-;
-;(vector! :categories :category)
-;
-;(boolean! :visible_on_listing)
-;
-;(pos-double! :unit_cost {:meta {:sensible-values [1.23 6.99 1443.32]}})
-;(enum! :unit_cost_basis ["per_person" "per_unit"] {:meta {:default "stuff"}})
-;
-;(string! :image_id {:min 4 :max 8})
-;(vector! :image_ids :image_id {:min 1 :max 1})
-;
-;(pos-double! :amount)
-;
-;(clone! :moid :charge_id)
-;(enum! :charge_type ["tax" "fee"])
-;(enum! :charge_unit ["%" "$"] {:default "%"})
-;
-;(clone! :moid :ref/vendor {:meta {:reference :vendor}})
-;(vector! :ref/vendors :ref/vendor {:meta {:reference :vendor}})
-;
-;(clone! :moid :ref/region {:meta {:reference :region}})
-;
-;(vector! :ref/regions :ref/region {:meta {:reference :region}})
-;
-;(document! :additional_charge
-;           [:label :amount :charge_type :charge_unit :charge_id])
-;
-;(vector! :additional_charges :additional_charge {:max 5})
-;
-;(string! :tag {:min 4 :max 12})
-;(vector! :tags :tag {:min 0 :max 8})
-;
-;(enum! :metro ["SFBay" "NYMetro" "LAMetro"])
-;
-;(set-of! :metros :metro)
-;
-;(document! :item/vendor
-;           [:ref/vendor :ref/region]
-;           [:unit_cost :unit_cost_basis :additional_charges])
-;
-;(vector! :item/vendors :item/vendor)
-;
-;(enum! :use_type ["Production" "Off-Site" "Event"])
-;
-;(document! :item
-;           [:_id :label :category :subcategory :active
-;            :visible_on_listing :unit_cost :unit_cost_basis]
-;           [:image_ids :additional_charges :metro :use_type :tags
-;            :ref/regions
-;            :item/vendors])
-;
-;(double! :latitude {:min -90.0 :max 90.0})
-;(double! :longitude {:min -180.0 :max 180.0})
-;
-;(enum! :geo/type ["Multipolygon"])
-;
-;(tuple! :long-lat [:longitude :latitude])
-;
-;(vector! :long-lats :long-lat {:min 3 :max 5})
-;
-;(vector! :geo/polygon :long-lats {:min 1 :max 1})
-;
-;(vector! :geo/coordinates :geo/polygon)
-;
-;(document! :area
-;           [:geo/type :geo/coordinates])
-;
-;(document! :region
-;           [:_id :label :active :area
-;            :metro :description]
-;           [:tags])
-;
-;(string! :contact {:min 2 :max 30 :meta {:sensible-values ["Jane Doe" "Carlos Ruis"]}})
-;(string! :address {:min 5 :meta {:sensible-values ["123 Main St." "245 W. 52nd" "1A Unicorn Way"]}})
-;(string! :address2 {:meta {:sensible-values ["Unit 101" "Suite 2A" "#42"]}})
-;(string! :city {:meta {:sensible-values ["San Francisco" "Brooklyn" "Austin" "Burbank"]}})
-;(string! :state {:min 2 :max 2 :meta {:label "State" :description "State or province" :hint "Enter state/province" :sensible-values ["CA" "NY" "TX"]}})
-;(string! :country {:meta {:sensible-values ["US"]}})
-;(string! :zip {:regex-pattern "\\d{5}"})
-;(string! :ein {:regex-pattern "\\d{2}-\\d{7}"})
-;(clone! :ein :tax_id)
-;(string! :email {:regex-pattern ".+@.+\\..{2,20}" :meta {:sensible-values ["jane@acme.org" "mulan@cater.io"]}})
-;(string! :phone {:min 10 :meta {:sensible-values ["(555) 555-1212" "(213) 555-1761"]}})
-;
-;(string! :vendor/name {:min 2 :max 30 :meta {:sensible-values ["Acme Hospitality" "Bars and Bells" "Frida's Good Eats"]}})
-;(clone! :area :service_area)
-;(document! :vendor
-;           [:_id :vendor/name :active :description
-;            :contact :address :city :state :zip :email :phone]
-;           [:address2 :service_area :country :metros :tax_id])
-
 (def dictionary
   [
 
@@ -161,6 +29,13 @@
    {:id :_id
     :meta {:description "MongoDB identifier"}
     :clone :moid}
+
+   {:id :is_concierge
+    :meta {:description "True if Peerspace add-on-item"}
+    :type :boolean}
+
+   {:id :peerspace_add_on
+    :clone :is_concierge}
 
    {:id :active
     :meta {:description "Active flag (true/false)"}
@@ -265,7 +140,7 @@
     :type :string
     :regex-pattern ".+@.+..{2,20}"}
 
-   {:id :name
+   {:id :label
     :meta {:description "Name or label"
            :sensible-values ["Name #1" "Name A" "Name as such"]}
     :type :string
@@ -487,8 +362,8 @@
    ;{:id :ref/vendors
    ; :clone :vendor_ids}
 
-   {:id :vendor/name
-    :meta {:description "Vendor name"
+   {:id :vendor/label
+    :meta {:description "Vendor name/label"
            :sensible-values ["Acme Hospitality" "Bars and Bells" "Frida's Good Eats"]}
     :type :string
     :min 2
@@ -521,38 +396,34 @@
     :type :document
     :required-un [:geo/type :geo/coordinates]}
 
-   ;{:id :category_subcategory
-   ; :type :document
-   ; :required-un [:name :category :subcategory]}
-   ;
-   ;{:id :category_subcategories
-   ; :meta {:description "Catego"}
-   ; :type :vector
-   ; :vector :category_subcategory}
+   {:id :category_subcategory
+    :type :document
+    :required-un [:label :name/category :subcategory :rank]}
+
+   {:id :category_subcategories
+    :meta {:description "Catego"}
+    :type :vector
+    :vector :category_subcategory}
 
    {:id :item
     :meta {:description "Inventory item"}
     :type :document
-    :required-un [:_id :active :name/category :name :subcategory :unit_cost
+    :required-un [:_id :active :name/category :label :subcategory :unit_cost
                   :unit_cost_basis :visible_on_listing]
-    :optional-un [:additional_charges :category_ids
-                  :description :image_ids :item/region_vendors :metro :notes :region_ids :tags :use_types]}
+    :optional-un [:additional_charges :category_ids :category_subcategory
+                  :description :image_ids :item/region_vendors :is_concierge
+                  :metro :notes :peerspace_add_on :region_ids :tags :use_types]}
 
    {:id :region
     :meta {:description "Region with geo JSON polygon"}
     :type :document
-    :required-un [:_id :active :region/area :description :metro :name]
+    :required-un [:_id :active :region/area :description :metro :label]
     :optional-un [:tags]}
-
-   ;{:id :service_area
-   ; :meta {:description "Service area as geo JSON polygon"}
-   ; :type :document
-   ; :required-un [:geo/type :geo/coordinates]}
 
    {:id :vendor
     :meta {:description "Vendors information"}
     :type :document
-    :required-un [:_id :vendor/name :active :description :contact :address :city
+    :required-un [:_id :vendor/label :active :description :contact :address :city
                   :state :zip :contact_email :phone]
     :optional-un [:address2 :country :metros :payment_id
                   :notification_email :tags :notes :image_ids]}
@@ -572,8 +443,9 @@
 
    {:id :category
     :type :document
-    :required-un [:_id :active :label/category :name :label/subcategory]
+    :required-un [:_id :active :label/category :label :label/subcategory]
     :optional-un [:notes :rank :tags]}
+
    ;{:id :x
    ; :meta {:label ""
    ;        :description ""
@@ -585,92 +457,3 @@
    ]
 
   )
-
-;(defn clean
-;  [elem]
-;  (dissoc elem :type :id))
-;
-;(defn import-document!
-;  "Import the document element `elem`."
-;  [elem]
-;  (let [{:keys [id required required-un optional optional-un]} elem]
-;    (document! id required required-un optional optional-un (clean elem) nil)))
-;
-;(defn import-clone!
-;  "Import a clone element `elem`."
-;  [elem]
-;  (clone! (:clone elem) (:id elem) (clean elem)))
-;
-;(defn import-element!
-;  "Import element `elem` with optional diction function `diction-f`."
-;  ([elem] (import-element! string! elem))
-;  ([diction-f elem]
-;   (diction-f (:id elem) elem)))
-;
-;(defn import-ofs!
-;  "Import collection of `of-key` elements for element `elem` with the diction
-;  `register-f` function."
-;  [register-f of-key elem]
-;  (let [{:keys [id]} elem
-;        of (get elem of-key)]
-;    (register-f id of (clean elem))))
-;
-;(def import-vector! (partial import-ofs! vector! :vector-of))
-;(def import-poly-vector! (partial import-ofs! poly-vector! :poly-vector-of))
-;(def import-set! (partial import-ofs! set-of! :set-of))
-;(def import-enum! (partial import-ofs! enum! :enum))
-;(def import-tuple! (partial import-ofs! tuple! :tuple))
-;
-;(def import-string! (partial import-element! string!))
-;(def import-double! (partial import-element! double!))
-;(def import-pos-double! (partial import-element! pos-double!))
-;(def import-neg-double! (partial import-element! neg-double!))
-;(def import-long! (partial import-element! long!))
-;(def import-float! (partial import-element! float!))
-;(def import-pos-float! (partial import-element! pos-float!))
-;(def import-neg-float! (partial import-element! neg-float!))
-;(def import-boolean! (partial import-element! boolean!))
-;
-;(def types->functions
-;  {:string import-string!
-;   :document import-document!
-;   :double import-double!
-;   :pos-double import-pos-double!
-;   :neg-double import-neg-double!
-;   :long import-long!
-;   :float import-float!
-;   :pos-float import-pos-float!
-;   :neg-float import-neg-float!
-;   :vector import-vector!
-;   :set import-set!
-;   :enum import-enum!
-;   :tuple import-tuple!
-;   :poly-vector import-poly-vector!
-;   :boolean import-boolean!
-;   :clone import-clone!})
-;
-;(defn register-element!
-;  "Register element `elem` using declarative data."
-;  [elem]
-;  (let [nelem (if (:clone elem)
-;                (assoc elem :type :clone)
-;                elem)
-;        type (:type nelem :string)
-;        register-f (get types->functions type import-string!)]
-;    (register-f elem)))
-;
-;(defn import!
-;  "Import element `elem` to diction data dictionary."
-;  [elem]
-;  (register-element! elem))
-;
-;(defn imports!
-;  "Imports elements `elements` to diction data dictionary."
-;  [elements]
-;  (let [res (reduce #(conj (or % []) (import! %2))
-;                    nil
-;                    elements)]
-;
-;    res))
-
-; (register-element! {:id :answer :type :long :min 40 :max 63 :meta {:description "The answers are out there."}})
