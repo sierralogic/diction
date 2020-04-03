751729102aacbc82977d54d57121b62d5c805c24
Greg Seaton <greg.seaton@sierralogic.com>
2020-04-03 11:04:24 -0700

    corrected home page

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject diction "0.2.1"
+(defproject diction "0.2.2"
   :description "Diction is a Clojure data dictionary with data shape definition/validation/generation with batteries included."
-  :url "http://sierralogic.com/project/diction"
+  :url "https://sierralogic.github.io/diction"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.10.0"]
